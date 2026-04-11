8c15e0eb7e2b1e9c3626d8bdc888a87b44f89fe9
AbdulrahmanFrontend <abdulrahman.abdelstar@gmail.com>
2026-04-11 11:24:45 +0200

    Build ucLabeledComboBox

---
+++ DevToolkit.Controls/ucLabeledCombBox.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace DevToolkit.Controls
+{
+    partial class ucLabeledCombBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpContainer = new System.Windows.Forms.TableLayoutPanel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.cmbValues = new System.Windows.Forms.ComboBox();
+            this.tlpContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpContainer
+            // 
+            this.tlpContainer.AutoSize = true;
+            this.tlpContainer.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tlpContainer.ColumnCount = 1;
+            this.tlpContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpContainer.Controls.Add(this.lblTitle, 0, 0);
+            this.tlpContainer.Controls.Add(this.cmbValues, 0, 1);
+            this.tlpContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpContainer.Location = new System.Drawing.Point(7, 7);
+            this.tlpContainer.Margin = new System.Windows.Forms.Padding(4);
+            this.tlpContainer.Name = "tlpContainer";
+            this.tlpContainer.RowCount = 2;
+            this.tlpContainer.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpContainer.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpContainer.Size = new System.Drawing.Size(127, 60);
+            this.tlpContainer.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(4, 0);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(42, 23);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title";
+            // 
+            // cmbValues
+            // 
+            this.cmbValues.FormattingEnabled = true;
+            this.cmbValues.Location = new System.Drawing.Point(3, 26);
+            this.cmbValues.Name = "cmbValues";
+            this.cmbValues.Size = new System.Drawing.Size(121, 31);
+            this.cmbValues.TabIndex = 1;
+            // 
+            // ucCombBoxField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.tlpContainer);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ucCombBoxField";
+            this.Padding = new System.Windows.Forms.Padding(7);
+            this.Size = new System.Drawing.Size(141, 74);
+            this.tlpContainer.ResumeLayout(false);
+            this.tlpContainer.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpContainer;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.ComboBox cmbValues;
+    }
+}
---
+++ DevToolkit.Controls/ucLabeledCombBox.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevToolkit.Controls
+{
+    public partial class ucLabeledCombBox : UserControl
+    {
+        public ucLabeledCombBox()
+        {
+            InitializeComponent();
+        }
+
+        [Category("Custom Properties")]
+        public string LabelText
+        {
+            get => lblTitle.Text;
+            set => lblTitle.Text = value;
+        }
+
+        [Category("Custom Properties")]
+        public AutoCompleteMode ComboBoxAutoCompleteMode
+        {
+            get => cmbValues.AutoCompleteMode;
+            set => cmbValues.AutoCompleteMode = value;
+        }
+
+        [Category("Custom Properties")]
+        public ComboBoxStyle ComboBoxDropDownStyle
+        {
+            get => cmbValues.DropDownStyle;
+            set => cmbValues.DropDownStyle = value;
+        }
+
+        [Category("Custom Properties")]
+        public bool SortedComboBoxItems
+        {
+            get => cmbValues.Sorted;
+            set => cmbValues.Sorted = value;
+        }
+
+        // SortedSet stores unique elements in sorted order so it is faster than List, HashSet
+        [Description("Sets the items of the ComboBox.")]
+        public void SetComboBoxItems(SortedSet<string> Items)
+        {
+            if (Items.Count > 0)
+            {
+                cmbValues.Items.Clear();
+                foreach (var Item in Items)
+                {
+                    if (Item != null)
+                    {
+                        cmbValues.Items.Add(Item);
+                    }
+                }
+                cmbValues.SelectedIndex = 0;
+            }
+        }
+
+        public Action<object> SelectItem;
+        protected void OnSelectedItem(object Item)
+        {
+            Action<object> handler = SelectItem;
+            if (handler != null)
+            {
+                handler(Item);
+            }
+        }
+
+        private void CCCmBValues_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(SelectItem != null)
+            {
+                OnSelectedItem(cmbValues.SelectedItem);
+            }
+        }
+    }
+}
