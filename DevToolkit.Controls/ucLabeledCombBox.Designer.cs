ac6baf3232ca8a8c2c3ba87666e7921d85cc67fa
AbdulrahmanFrontend <abdulrahman.abdelstar@gmail.com>
2026-04-13 09:59:23 +0200

    Improving ucLabeledComboBox

--- DevToolkit.Controls/ucLabeledCombBox.Designer.cs
+++ DevToolkit.Controls/ucLabeledCombBox.Designer.cs
@@ -28,29 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tlpContainer = new System.Windows.Forms.TableLayoutPanel();
-            this.lblTitle = new System.Windows.Forms.Label();
             this.cmbValues = new System.Windows.Forms.ComboBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.tlpContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // tlpContainer
             // 
+            this.tlpContainer.AutoSize = true;
             this.tlpContainer.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tlpContainer.ColumnCount = 1;
             this.tlpContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpContainer.Controls.Add(this.lblTitle, 0, 0);
             this.tlpContainer.Controls.Add(this.cmbValues, 0, 1);
+            this.tlpContainer.Controls.Add(this.lblTitle, 0, 0);
             this.tlpContainer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlpContainer.Location = new System.Drawing.Point(1, 1);
             this.tlpContainer.Margin = new System.Windows.Forms.Padding(4);
             this.tlpContainer.Name = "tlpContainer";
-            this.tlpContainer.Padding = new System.Windows.Forms.Padding(1, 1, 1, 2);
+            this.tlpContainer.Padding = new System.Windows.Forms.Padding(1);
             this.tlpContainer.RowCount = 2;
             this.tlpContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tlpContainer.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tlpContainer.Size = new System.Drawing.Size(310, 69);
+            this.tlpContainer.Size = new System.Drawing.Size(310, 66);
             this.tlpContainer.TabIndex = 0;
+            // 
+            // cmbValues
+            // 
+            this.cmbValues.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cmbValues.FormattingEnabled = true;
+            this.cmbValues.Location = new System.Drawing.Point(4, 31);
+            this.cmbValues.Name = "cmbValues";
+            this.cmbValues.Size = new System.Drawing.Size(302, 31);
+            this.cmbValues.TabIndex = 1;
             // 
             // lblTitle
             // 
@@ -62,14 +75,9 @@
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Title";
             // 
-            // cmbValues
+            // errorProvider1
             // 
-            this.cmbValues.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cmbValues.FormattingEnabled = true;
-            this.cmbValues.Location = new System.Drawing.Point(4, 33);
-            this.cmbValues.Name = "cmbValues";
-            this.cmbValues.Size = new System.Drawing.Size(302, 31);
-            this.cmbValues.TabIndex = 1;
+            this.errorProvider1.ContainerControl = this;
             // 
             // ucLabeledCombBox
             // 
@@ -81,10 +89,12 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ucLabeledCombBox";
             this.Padding = new System.Windows.Forms.Padding(1);
-            this.Size = new System.Drawing.Size(312, 71);
+            this.Size = new System.Drawing.Size(312, 68);
             this.tlpContainer.ResumeLayout(false);
             this.tlpContainer.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -93,5 +103,6 @@
         private System.Windows.Forms.TableLayoutPanel tlpContainer;
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.ComboBox cmbValues;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- DevToolkit.Controls/ucLabeledCombBox.cs
+++ DevToolkit.Controls/ucLabeledCombBox.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevToolkit.Core;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,9 +26,85 @@ namespace DevToolkit.Controls
         }
 
         [Category("Custom Properties")]
-        public bool Required { get; set; }
-        
+        public Font ValuesFont
+        {
+            get => cmbValues.Font;
+            set => cmbValues.Font = value;
+        }
+
         [Category("Custom Properties")]
-        public ComboBox ComboBoxValues => cmbValues;
+        public bool Required { get; set; } = false;
+
+        [Category("Custom Properties")]
+        public AutoCompleteMode AutoCompleteMode
+        {
+            get => cmbValues.AutoCompleteMode;
+            set => cmbValues.AutoCompleteMode = value;
+        }
+
+        [Category("Custom Properties")]
+        public AutoCompleteSource AutoCompleteSource
+        {
+            get => cmbValues.AutoCompleteSource;
+            set => cmbValues.AutoCompleteSource = value;
+        }
+
+        [Category("Custom Properties")]
+        public ComboBoxStyle DropDownStyle
+        {
+            get => cmbValues.DropDownStyle;
+            set => cmbValues.DropDownStyle = value;
+        }
+
+        [Category("Custom Properties")]
+        public bool Sorted
+        {
+            get => cmbValues.Sorted;
+            set => cmbValues.Sorted = value;
+        }
+
+        [Category("Custom Properties")]
+        public bool HasDefaultValue { get; set; } = false;
+
+        [Category("Custom Properties")]
+        public string DefaultValue { get; set; } = string.Empty;
+
+        [Description("Get clsResult.Success(string Message) if it is valid " +
+            "or get clsResult.Failure(string Message) if it is not valid.")]
+        public clsResult Valid()
+        {
+            if (Required && string.IsNullOrEmpty(cmbValues.Text))
+            {
+                return clsResult.Failure("Requried");
+            }
+            return clsResult.Success();
+        }
+
+        [Category("Custom Properties")]
+        public string SelectedItem => cmbValues.SelectedItem?.ToString();
+
+        [Description("Fill ComboBox")]
+        public void SetValues(List<string> Values)
+        {
+            if(Values.Count > 0)
+            {
+                cmbValues.Items.Clear();
+                foreach (string Value in Values)
+                {
+                    cmbValues.Items.Add(Value);
+                }
+                if (HasDefaultValue)
+                {
+                    if (string.IsNullOrEmpty(DefaultValue))
+                    {
+                        cmbValues.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        cmbValues.SelectedItem = DefaultValue;
+                    }
+                }
+            }
+        }
     }
 }
