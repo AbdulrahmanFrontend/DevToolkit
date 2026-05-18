df2c27215065ab6b7d009add81c792a5a41891d1
AbdulrahmanFrontend <abdulrahman.abdelstar@gmail.com>
2026-05-18 15:42:31 +0300

    Build DbProviderFactory

---
+++ DevToolkit.Data/Providers/DbProviderFactory.cs
@@ -0,0 +1,31 @@
+﻿using DevToolkit.Data.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevToolkit.Data.Providers
+{
+    public class DbProviderFactory
+    {
+        public enum DbProvider
+        {
+            SqlServer,
+            SQLite
+        }
+
+        public static IDbExecutor Create(DbProvider Provider)
+        {
+            switch (Provider)
+            {
+                case DbProvider.SqlServer:
+                    return new Executors.SqlServerExecutor();
+                case DbProvider.SQLite:
+                    return new Executors.SQLiteExecutor();
+                default:
+                    throw new NotSupportedException($"The provider {Provider} is not supported.");
+            }
+        }
+    }
+}
